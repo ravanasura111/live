81b45a7fc970b8cc8532daa9a2c8148da9961666
ramesh kumar tadivada <rameshkumariosapp@gmail.com>
2019-08-17 12:35:56 +0530

    Add proxy configuration, adds rental detail styles improvements, and adds pipes

--- src/app/components/rental/rental.module.ts
+++ src/app/components/rental/rental.module.ts
@@ -1,11 +1,15 @@
 import { NgModule } from '@angular/core';
 import { RentalComponent } from './rental.component';
 import { CommonModule } from '@angular/common';
+import { NgPipesModule } from 'ngx-pipes';
+import { UppercasePipe } from '../pipes/uppercase.pipe';
+
 import { RentalListComponent } from './rental-list/rental-list.component';
 import { RentalListItemsComponent } from './rental-list-items/rental-list-items.component';
 import { Routes, RouterModule } from '@angular/router';
 import { RentalService } from '../../services/rental.service';
 import { RentalDetailsComponent } from '../rental-details/rental-details.component';
+import { HttpClientModule } from '@angular/common/http';
 const routes: Routes = [
     { path: 'rentals', 
       component: RentalComponent,
@@ -20,10 +24,10 @@ const routes: Routes = [
         RentalComponent,
         RentalListComponent,
         RentalListItemsComponent,
-        RentalDetailsComponent
+        RentalDetailsComponent,UppercasePipe
     ],
     providers:[RentalService],
-    imports: [RouterModule.forRoot(routes),CommonModule],
+    imports: [RouterModule.forRoot(routes),CommonModule,HttpClientModule,NgPipesModule],
 })
 export class RentalModule {
 
